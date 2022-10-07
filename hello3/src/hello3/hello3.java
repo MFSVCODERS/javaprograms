cc0de62924fe23b69cbb4e91e60837be5777dfc5
MFSVCODERS <114238920+MFSVCODERS@users.noreply.github.com>
2022-10-07 15:36:58 +0530

    Add files via upload

---
+++ hello3/src/hello3/hello3.java
@@ -0,0 +1,12 @@
+package hello3;
+
+public class hello3 {
+	public static void main(String args[])
+	{
+		boolean var=false;
+		System.out.print(var);
+		char var1='\u00A7';
+				System.out.print(var1);
+	}
+
+}
