40114f09731c719670254685418a0bc516ba0289
MFSVCODERS <114238920+MFSVCODERS@users.noreply.github.com>
2022-10-07 15:30:26 +0530

    Add files via upload

---
+++ typecasting/src/typecasting/typecasting.java
@@ -0,0 +1,28 @@
+package typecasting;
+
+public class typecasting {
+		public static void main(String args[])
+		{
+			//implicit
+			float f=10.532f;
+			long l=(long) f;
+			System.out.print(l);
+			
+			//narrowing
+			byte b=58;
+			short s=b;
+			System.out.print(s);
+			
+			float f1=22.34f;
+			double d=f1;
+			System.out.print(f1);
+			System.out.print(d);
+			
+			
+		
+		}
+		
+
+	}
+
+}
